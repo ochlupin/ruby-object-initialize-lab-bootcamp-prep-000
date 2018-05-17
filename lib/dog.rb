453ab42277588a9b356db5274ed7b4cde695d3a5
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-05-17 15:15:31 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  def initialize(dog_name, breed="Mutt")
+    @name = dog_name
+    @breed = breed
+  end
+
+  def name =(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed=breed
+  end
+
+  def breed
+    @breed
+  end
+    
+end
